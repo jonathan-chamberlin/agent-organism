ce13c1afd6384c709b75ef64cf886d6420dae1d8
Jonathan Chamberlin <jcham17x@gmail.com>
2025-11-22 15:58:33 -0500

    changed coordinates_to_q_table_index to accept a tuple of x and y coordinates instead of 2 seperate int inputs

--- start.py
+++ start.py
@@ -13,9 +13,6 @@ q_table_width = directions_agent_can_move
 
 q_table = np.zeros((environment_x_length*environment_y_length,directions_agent_can_move))
 
-print(q_table)
-
-
 direction_map = {
     "up": (0,-1),
     "down":(0,1),
@@ -24,20 +21,26 @@ direction_map = {
     }
 
 
-'''A function that converts (x, y) to Q-table row index
+'''
+Plan for which functions to create to allow the agent to move across the environment.
 
-A function that takes current position and direction, returns new position
+1. A function that converts (x, y) to Q-table row index
 
-A function that checks if a move is valid (not hitting a wall)
+2. A function that takes current position and direction, returns new position
 
-A function that updates the Q-table after learning'''
+3. A function that checks if a move is valid (not hitting a wall)
 
-def coordinates_to_q_table_index(x_coord: int, y_coord: int) -> int:
+4. A function that updates the Q-table after learning'''
+
+def coordinates_to_q_table_index(coordinates: tuple[int, int]) -> int:
     '''A function that converts (x, y) to Q-table row index. If the x and y coordinates are outside the environment's dimensions, the function returns -1'''
     
     global q_table_width
     global environment_x_length
     global environment_y_length
+    
+    x_coord = coordinates[0]
+    y_coord = coordinates[1]
     
     if (x_coord > environment_x_length) or (y_coord > environment_y_length) or (x_coord < 0) or (y_coord < 0 ):
         return -1
@@ -46,3 +49,8 @@ def coordinates_to_q_table_index(x_coord: int, y_coord: int) -> int:
     
     
     return row_index
+
+
+def coordinates_after_moving(x_coord, y_coord, direction) -> tuple[int, int]:
+    """A function that takes current position and direction, returns new position"""
+    return 0
--- test_start.py
+++ test_start.py
@@ -3,15 +3,15 @@ from start import q_table_width
 
 
 def test_coordinates_to_q_table_index() -> None:
-    assert coordinates_to_q_table_index(0,0) == 0
-    assert coordinates_to_q_table_index(1,0) == 1 + 0 * q_table_width
-    assert coordinates_to_q_table_index(0,1) == 0 + 1 * q_table_width
-    assert coordinates_to_q_table_index(5,1) == 5 + 1 * q_table_width
-    assert coordinates_to_q_table_index(5,9) == 5 + 9 * q_table_width
-    assert coordinates_to_q_table_index(-5,0) == -1
-    assert coordinates_to_q_table_index(0,-5) == -1
-    assert coordinates_to_q_table_index(11,0) == -1
-    assert coordinates_to_q_table_index(0,11) == -1
-    assert coordinates_to_q_table_index(-5,-50) == -1
+    assert coordinates_to_q_table_index([0,0]) == 0
+    assert coordinates_to_q_table_index([1,0]) == 1 + 0 * q_table_width
+    assert coordinates_to_q_table_index([0,1]) == 0 + 1 * q_table_width
+    assert coordinates_to_q_table_index([5,1]) == 5 + 1 * q_table_width
+    assert coordinates_to_q_table_index([5,9]) == 5 + 9 * q_table_width
+    assert coordinates_to_q_table_index([-5,0]) == -1
+    assert coordinates_to_q_table_index([0,-5]) == -1
+    assert coordinates_to_q_table_index([11,0]) == -1
+    assert coordinates_to_q_table_index([0,11]) == -1
+    assert coordinates_to_q_table_index([-5,-50]) == -1
 
     
