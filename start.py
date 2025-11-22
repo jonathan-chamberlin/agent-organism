bb8b38ceabe4b580b571b52eed0f21f3caadf9d3
Jonathan Chamberlin <jcham17x@gmail.com>
2025-11-22 15:48:58 -0500

    working function coordinates_to_q_table

--- start.py
+++ start.py
@@ -23,3 +23,26 @@ direction_map = {
     "right":(1,0)
     }
 
+
+'''A function that converts (x, y) to Q-table row index
+
+A function that takes current position and direction, returns new position
+
+A function that checks if a move is valid (not hitting a wall)
+
+A function that updates the Q-table after learning'''
+
+def coordinates_to_q_table_index(x_coord: int, y_coord: int) -> int:
+    '''A function that converts (x, y) to Q-table row index. If the x and y coordinates are outside the environment's dimensions, the function returns -1'''
+    
+    global q_table_width
+    global environment_x_length
+    global environment_y_length
+    
+    if (x_coord > environment_x_length) or (y_coord > environment_y_length) or (x_coord < 0) or (y_coord < 0 ):
+        return -1
+    
+    row_index = x_coord + y_coord * q_table_width
+    
+    
+    return row_index
