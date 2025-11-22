fda23b47e70faa1793ad30bffb9adbf61f145c51
Jonathan Chamberlin <jcham17x@gmail.com>
2025-11-22 16:22:48 -0500

    working function coordinates_after_moving which given coordinates and a direction, returns the new coordinates, as long as the initial and new coordinates are inside the environment

--- start.py
+++ start.py
@@ -51,6 +51,42 @@ def coordinates_to_q_table_index(coordinates: tuple[int, int]) -> int:
     return row_index
 
 
-def coordinates_after_moving(x_coord, y_coord, direction) -> tuple[int, int]:
-    """A function that takes current position and direction, returns new position"""
-    return 0
+
+print([5,5] + [0,1])
+# results in [5,5,0,1]
+
+
+def coordinates_after_moving(coordinates: tuple[int, int], direction: str) -> tuple[int, int]:
+    """A function that takes current position and direction, returns new position.
+    
+    If the inputted coordinates are outside the environmet, the function returns [-1.-1].
+    
+    If the movement takes the agent outside the environment, the function returns [-5,-5].  """
+    
+    global q_table_width
+    global environment_x_length
+    global environment_y_length
+    global direction_map
+    
+    x_coord = coordinates[0]
+    y_coord = coordinates[1]
+    
+    # checking if the inputted coordinates are inside the environment
+    if (x_coord > environment_x_length) or (y_coord > environment_y_length) or (x_coord < 0) or (y_coord < 0 ):
+        return [-1,-1]
+    
+    # creating new coordinates
+    
+    new_x_coord = x_coord + direction_map[direction][0]
+    new_y_coord = y_coord + direction_map[direction][1]
+
+    
+    # checking if the resulting coordinates are inside the environment
+    if (new_x_coord > environment_x_length) or (new_y_coord > environment_y_length) or (new_x_coord < 0) or (new_y_coord < 0 ):
+        return [-5,-5]
+    
+    # at this point, since everything has been checked and the movement is valid, we create the new tuple (different place in memory) representing the new coordinates after the movement.
+    
+    new_coords = [new_x_coord, new_y_coord]
+
+    return new_coords
--- test_start.py
+++ test_start.py
@@ -1,5 +1,6 @@
 from start import coordinates_to_q_table_index
 from start import q_table_width
+from start import coordinates_after_moving
 
 
 def test_coordinates_to_q_table_index() -> None:
@@ -14,4 +15,9 @@ def test_coordinates_to_q_table_index() -> None:
     assert coordinates_to_q_table_index([0,11]) == -1
     assert coordinates_to_q_table_index([-5,-50]) == -1
 
-    
+def test_coordinates_after_moving() -> None:
+    assert coordinates_after_moving([0,0],"down") == [0,1]
+    assert coordinates_after_moving([0,0],"right") == [1,0]
+    assert coordinates_after_moving([1,1],"left") == [0,1]
+    assert coordinates_after_moving([0,0],"right") == [1,0]
+    assert coordinates_after_moving([1,1],"up") == [1,0]
