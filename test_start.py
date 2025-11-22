0ec5965bc86803da118489ef3c07700f698e4560
Jonathan Chamberlin <jcham17x@gmail.com>
2025-11-22 16:36:17 -0500

    modified tests to ensure boundary conditons are accurately checked

--- test_start.py
+++ test_start.py
@@ -11,7 +11,7 @@ def test_coordinates_to_q_table_index() -> None:
     assert coordinates_to_q_table_index([5,9]) == 5 + 9 * q_table_width
     assert coordinates_to_q_table_index([-5,0]) == -1
     assert coordinates_to_q_table_index([0,-5]) == -1
-    assert coordinates_to_q_table_index([11,0]) == -1
+    assert coordinates_to_q_table_index([10,0]) == -1
     assert coordinates_to_q_table_index([0,11]) == -1
     assert coordinates_to_q_table_index([-5,-50]) == -1
 
@@ -19,4 +19,8 @@ def test_coordinates_after_moving() -> None:
     assert coordinates_after_moving((0,0),"down") == (0,1)
     assert coordinates_after_moving((0,0),"right") == (1,0)
     assert coordinates_after_moving((1,1),"left") == (0,1)
-    assert coordinates_after_moving((0,0),"right") == (1,0)
+    assert coordinates_after_moving((1,1),"left") == (0,1)
+    assert coordinates_after_moving((0,0),"up") == (-5,-5)
+    assert coordinates_after_moving((9,0),"right") == (-5,-5)
+    assert coordinates_after_moving((10,0),"left") == (-1,-1)
+    assert coordinates_after_moving((0,10),"up") == (-1,-1)
