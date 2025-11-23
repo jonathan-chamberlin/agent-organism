1cb64fba7edfa17ce1ef4f407d68204ca3c0a829
Jonathan Chamberlin <jcham17x@gmail.com>
2025-11-22 22:05:51 -0500

    adding documentation about hwo add_custom_object is a more general version of add_walls and add_goals

--- start.py
+++ start.py
@@ -105,6 +105,26 @@ def add_goals(maze_grid, goal_cells: list[tuple]) -> ndarray:
     
     return copied_maze_grid_with_goals
 
+def add_custom_object(maze_grid, cells_to_put_object_in: list[tuple], chosen_value) -> ndarray:
+    """Takes in a list of cells in the form a a tuple. The cells are in the form (x_coord, y_coord). The output is a modification of the maze array where every cell in list of wall cells sets the value of the maze to chosen_value.
+    
+    Because of aliasing, this will not modify whatever maze_grid is inputted.
+    """
+    
+    # This makes sure that the orignal maze_grid isn't changed in memory.
+    copied_maze_grid_with_custom_objects = maze_grid.copy()
+    
+    for cell in cells_to_put_object_in:
+        #get the x and y coords of the wall
+        x_coord = cell[0]
+        y_coord = cell[1]
+        
+        # now go into the maze_grid and find the place in it that is in the same place as the wall. Then set that equal to the wall_value
+        copied_maze_grid_with_custom_objects[y_coord][x_coord] = chosen_value
+    
+    
+    return copied_maze_grid_with_custom_objects
+
 def coordinates_after_moving(coordinates: tuple[int, int], direction: str, walls: list[tuple[int,int]]) -> tuple[tuple[int, int],bool]:
     """A function that takes current position and direction, returns a tuple. The first item of the tuple is the new position as a tuple of (x,y), and the second item in that tuple is whether or not it's a valid move, meaning if the agent hits a wall or exists the environment. 
     
@@ -149,25 +169,12 @@ def coordinates_after_moving(coordinates: tuple[int, int], direction: str, walls
 # For learning, I need to create A function that updates the Q-table after learning
 # For display, I need to print out the maze array with the addition of walls and goals.
 
+
+
+'''By seeing that this evaluates to true, it's clear that add_custom_object is a generalized application of add_walls and add_gaols
+print(np.array_equal((add_walls(add_goals(maze,goals),walls)),add_custom_object(add_custom_object(maze, goals, goal_value),walls,wall_value)))
+
 print(add_walls(add_goals(maze,goals),walls))
 
-
-def add_custom_object(maze_grid, cells_to_put_object_in: list[tuple], chosen_value) -> ndarray:
-    """Takes in a list of cells in the form a a tuple. The cells are in the form (x_coord, y_coord). The output is a modification of the maze array where every cell in list of wall cells sets the value of the maze to chosen_value.
-    
-    Because of aliasing, this will not modify whatever maze_grid is inputted.
-    """
-    
-    # This makes sure that the orignal maze_grid isn't changed in memory.
-    copied_maze_grid_with_custom_objects = maze_grid.copy()
-    
-    for cell in cells_to_put_object_in:
-        #get the x and y coords of the wall
-        x_coord = cell[0]
-        y_coord = cell[1]
-        
-        # now go into the maze_grid and find the place in it that is in the same place as the wall. Then set that equal to the wall_value
-        copied_maze_grid_with_custom_objects[y_coord][x_coord] = chosen_value
-    
-    
-    return copied_maze_grid_with_custom_objects
+print(add_custom_object(add_custom_object(maze, goals, goal_value),walls,wall_value))
+'''
