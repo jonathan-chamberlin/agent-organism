63663d60b0d1e804fbd0612149c2897b12c9c010
Jonathan Chamberlin <jcham17x@gmail.com>
2025-11-24 14:05:27 -0500

    working using draw_agent to put agent at the start cell

--- setup_environment.py
+++ setup_environment.py
@@ -124,9 +124,10 @@ def draw_grid(grid: tuple[tuple[int,int]], object_coloring: map)-> None:
 draw_grid(full_environment, cell_color_map)
 
 # draw agent on start square
-circle_centered_on_start_coords = start
-agent_at_start_location = coords_to_center_of_cell_in_pixels(start[0])
-pg.draw.circle(window, agent_color, agent_at_start_location, agent_width, agent_height)
+draw_agent((start)[0])
+# circle_centered_on_start_coords = start
+# agent_at_start_location = coords_to_center_of_cell_in_pixels(start[0])
+# pg.draw.circle(window, agent_color, agent_at_start_location, agent_width, agent_height)
 
 # renders everything
 pg.display.flip()
