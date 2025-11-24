6424d0723fbbcf6626e78bb3c3b5eacb0863b9e8
Jonathan Chamberlin <jcham17x@gmail.com>
2025-11-24 14:08:24 -0500

    game loop is now only in main.py file

--- setup_environment.py
+++ setup_environment.py
@@ -132,8 +132,4 @@ draw_agent(start)
 # renders everything
 pg.display.flip()
 
-running = True
-while running:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
+
