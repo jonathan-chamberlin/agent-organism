9a6a3297417ba6bf9a622c5c0c1123f51af4fcf0
Jonathan Chamberlin <jcham17x@gmail.com>
2025-11-24 15:22:33 -0500

    listing out a way to improve my game_loop function

--- main.py
+++ main.py
@@ -3,27 +3,6 @@ from setup_environment import *
 
 moves = ["down", "down", "down", "right", "right","right","up","up","up","right","right"]
 
-# Game loop
-"""
-agent_coords = start
-draw_grid_and_background(full_environment, cell_color_map, background_color)
-draw_agent(agent_coords)
-pg.display.flip()
-pg.time.delay(delay_in_ms_for_framerate)
-
-for move in moves:
-    draw_grid_and_background(full_environment, cell_color_map, background_color)
-    coords_calc = coordinates_after_moving(agent_coords,move,walls)
-    next_coords = coords_calc[0]
-    movement_valid = coords_calc[1] 
-    draw_agent(next_coords)
-    pg.display.flip()
-    agent_coords = next_coords
-    pg.time.delay(delay_in_ms_for_framerate)
-"""
-
-# left off. create funcion that takes in list of moves, and then renders everything
-
 game_loop(full_environment, start, walls, cell_color_map, background_color, moves)
 
 running = True
--- setup_environment.py
+++ setup_environment.py
@@ -137,7 +137,10 @@ def draw_grid_and_background(grid: tuple[tuple[int,int]], object_coloring: map, 
         draw_grid(grid, object_coloring)
 
 def game_loop(environment: tuple[tuple[int,int]], start: tuple[int,int], walls: list[tuple(int,int)], object_coloring: map, color_for_background, moves: list[str]) -> list[bool]:
-    """Takes in a bunch of inputs, and for every move it draws the full environment (grid and backgroun), then draws the agent, then calculates its next move nad position, then checks if that next position would be valid, then draws it. """
+    """Takes in a bunch of inputs, and for every move it draws the full environment (grid and backgroun), then draws the agent, then calculates its next move nad position, then checks if that next position would be valid, then draws it, and renders it.
+    
+    Improvement: The data for start and walls is inside the full_environment variable that is inputted into this function, so teh inputs of start and walls are redundant. I could write code to look at the environment input and find which cell has the start_value and store those coords as a tuple, and which cells have the wall_value and store that as a list of tuples, and then use those variables in the code below.
+    """
     
     movement_valid_list = []
 
