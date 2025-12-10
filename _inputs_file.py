7e3ff280c9f589c9f1c17f501c8ae438777f66c2
Jonathan Chamberlin <jcham17x@gmail.com>
2025-12-10 02:58:51 -0500

    changed some inputs

--- _inputs_file.py
+++ _inputs_file.py
@@ -9,14 +9,14 @@ pg.init()
 possible_actions = [(1,0), (0,1),(-1,0),(0,-1),(0,0)]
 actions_to_execute = [(1,0)] #only when you want to have the agent execute a list of predetermined actions, you would plug this into game_loop_manual
 
-runs = 6
-action_limit = 8
-framerate = 6
+runs = 5
+action_limit = 50
+framerate = 25
 
-environment_row_count = 6
-environment_column_count = 6
+environment_row_count = 9
+environment_column_count = 9
 
-goals = [(4,4)]
+goals = [(7,7)]
 start_list = [(1,1)]
 walls_input = []
 
