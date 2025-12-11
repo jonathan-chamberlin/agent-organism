b3bbf4a949c9d5731eb02ff0c1574d6956bb11d8
Jonathan Chamberlin <jcham17x@gmail.com>
2025-12-11 18:38:14 -0500

    formatting

--- _inputs_file.py
+++ _inputs_file.py
@@ -10,13 +10,13 @@ possible_actions = [(1,0), (0,1),(-1,0),(0,-1),(0,0), (1,1),(1,-1),(-1,1),(-1,-1
 actions_to_execute = [(1,0)] #only when you want to have the agent execute a list of predetermined actions, you would plug this into game_loop_manual
 
 runs = 5
-action_limit = 75
-framerate = 60
+action_limit = 50
+framerate = 30
 
-environment_row_count = 14
-environment_column_count = 14
+environment_row_count = 8
+environment_column_count = 8
 
-goals = [(12,12)]
+goals = [(6,6)]
 start_list = [(1,1)]
 walls_input = [(2,2),(2,3),(3,2)]
 
